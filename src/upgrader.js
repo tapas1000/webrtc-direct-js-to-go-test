aaacef5c1f2acbae6ffc40b945b9ef35be7eba75
tapas1000 <tapas@swarmlabs.io>
2020-09-16 13:57:03 +0530

    bug fix

--- src/upgrader.js
+++ src/upgrader.js
@@ -7,11 +7,13 @@ class Upgrader {
   
   static async upgradeInbound (maConn) {
     console.log(maConn);
+    return maConn;
   }
 
   
  static async upgradeOutbound (maConn) {
     console.log(maConn);
+    return maConn;
  }
 }
 

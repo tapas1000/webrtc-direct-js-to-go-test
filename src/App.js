0bf74a3283c51565625cd23918972fcae34085f7
tapas1000 <tapas@swarmlabs.io>
2020-09-16 14:48:41 +0530

    created build

--- src/App.js
+++ src/App.js
@@ -12,42 +12,24 @@ flags.defineBoolean(listenFlag, false, 'Listen for incoming connections.')
 flags.parse()
 const listening = flags.get(listenFlag)
 const maddr = multiaddr('/ip4/34.122.48.103/tcp/9090/http/p2p-webrtc-direct')
-const direct = new WebRTCDirect({ upgrader })
-if (listening) {
-  const listener = direct.createListener({ config: {} }, (conn) => {
-    console.log('[listener] Got connection')
-    const muxer = mplex.listener(conn)
-    muxer.on('stream', (stream) => {
-      console.log('[listener] Got stream')
-      pull(
-      stream,
-      pull.drain((data) => {
-        console.log('[listener] Received:')
-        console.log(data.toString())
-      })
-      )
-    })
+const direct = new WebRTCDirect()
+
+direct.dial(maddr, { config: {} }, (err, conn) => {
+  if (err) {
+    console.log(`[dialer] Failed to open connection: ${err}`)
+  }
+  console.log('[dialer] Opened connection')
+  const muxer = mplex.dialer(conn)
+  const stream = muxer.newStream((err) => {
+    console.log('[dialer] Opened stream')
+    if (err) throw err
   })
-  listener.listen(maddr, () => {
-    console.log('[listener] Listening')
-  })
-} else {
-  direct.dial(maddr, { config: {} }, (err, conn) => {
-    if (err) {
-      console.log(`[dialer] Failed to open connection: ${err}`)
-    }
-    console.log('[dialer] Opened connection')
-    const muxer = mplex.dialer(conn)
-    const stream = muxer.newStream((err) => {
-      console.log('[dialer] Opened stream')
-      if (err) throw err
-    })
-    pull(
-      pull.values(['hey, how is it going. I am the dialer']),
-      stream
-    )
-  })
-}
+  pull(
+    pull.values(['hey, how is it going. I am the dialer']),
+    stream
+  )
+})
+
 
 
 function App() {
